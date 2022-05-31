29aee8b0f2e37e37ef8ea84e063d4f910b30358c
Angel Perez <dev.angel.perez@gmail.com>
2022-05-30 22:31:33 -0500

    Proyecto OK

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,14 +3,8 @@
     <v-row>
       <v-col cols="12">
         <v-carousel v-model="model">
-          <v-carousel-item v-for="(color, i) in colors" :key="color">
-            <v-sheet :color="color" height="100%" tile>
-              <v-row class="fill-height" align="center" justify="center">
-                <div class="text-h2">
-                  Imagen {{ i + 1 }}
-                </div>
-              </v-row>
-            </v-sheet>
+          <v-carousel-item v-for="(imagen, i) in imagenes" :key="i" :src="imagen.src"
+            reverse-transition="fade-transition" transition="fade-transition">
           </v-carousel-item>
         </v-carousel>
       </v-col>
@@ -24,12 +18,19 @@ export default {
 
   data: () => ({
     model: 0,
-    colors: [
-      'green darken-1',
-      'green darken-2',
-      'green darken-3',
-      'green darken-4',
-      'green darken-5',
+    imagenes: [
+      {
+        src: '/imagenchidori1.jpg',
+      },
+      {
+        src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+      },
+      {
+        src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg',
+      },
+      {
+        src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg',
+      },
     ],
   }),
 }
